1cd56e0acc7d1f3cae505e2f5cee4eadb9240639
Yulia Karpova <karpova.jul@gmail.com>
2019-12-17 10:54:30 +1100

    Подключает полифил picturefill и плагин svg4everybody для корректного отображения картинок в ie11

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -33,5 +34,22 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("remove", function () {
+  return del(["source/js/lib"]);
+});
+
+gulp.task("modules", function () {
+  return gulp.src(
+    ["node_modules/picturefill/dist/picturefill.min.js", "node_modules/svg4everybody/dist/svg4everybody.min.js"]
+  )
+  .pipe(gulp.dest("source/js/lib"))
+  .pipe(server.stream());
+});
+
+gulp.task("js", gulp.series(
+  "remove",
+  "modules"
+));
 
 gulp.task("start", gulp.series("css", "server"));
